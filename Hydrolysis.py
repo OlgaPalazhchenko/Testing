f695d0c4ecbe4d3c78b89cb091eb87ae41f92d19
OlgaPalazhchenko <opalazhc@unb.ca>
2021-06-23 17:50:18 -0300

    added instructions to put hydrolysis constants into function form

--- Hydrolysis.py
+++ Hydrolysis.py
@@ -5,10 +5,11 @@ Created on Jun 11, 2021
 '''
 import numpy as np
 
+
 #constants
 Temperature_K = 293 # K
 
-# These are in polynomial for, pulled from a thesis...
+
 # these solve for the log form of the hydrolysis constants for iron,
 # e.g.,
 
@@ -36,70 +37,36 @@ KFeOH2POLYNOMIAL = [-4E-10, 8.63467E-07, -0.00073731, 0.311499, -67.8248]
 KFeOH3POLYNOMIAL = [
     -4.667E-10, 1.0496E-06, -0.000935775, 0.413186, -97.4709
     ]
-# Above polynomials are used to evaluate the eq'm constants via polyval  
-# from numpy: 
-# https://numpy.org/doc/stable/reference/generated/numpy.polyval.html
-# Read up on polyval.
-
-#old method, polyval
-K_FeOH_hydr = 10 ** (np.polyval(KFeOHPOLYNOMIAL, Temperature_K))
-K_FeOH2_hydr = 10 ** (np.polyval(KFeOH2POLYNOMIAL, Temperature_K))
-K_FeOH3_hydr = 10 ** (np.polyval(KFeOH3POLYNOMIAL, Temperature_K))
-
-# Evaluating the above constants at a temperature from the Tremaine & LeBlanc
-# paper to make sure they are correct 
-
-import math
-
-print('The logarithmic equilibrium constants at a temperature of' , Temperature_K,'K' '\n'
-'from the Tremaine & LeBlanc paper using the old method, polyval, for' '\n'
-'FeOH, FeOH2 and FeOH3, respectively, are:')
-print(math.log10(K_FeOH_hydr), ',' ,math.log10(K_FeOH2_hydr), 'and', 
-    math.log10(K_FeOH3_hydr), '.',)
-
-#Making a code using the new method that puts them into a function form
-#The input is temperature, the output is the constants
-
-#example of new function, poly1d
-#K_FeOH_hydr = np.poly1d(KFeOHPOLYNOMIAL)
-#K_FeOH_hydr = 10 ** K_FeOH_hydr(Temperature_K)
 
 
-while True:
-    input_temperature = int(input('\n'
-'Input one of the following temperatures (in Kelvin): '
-'373, 423, 473, 523, 573' 
-'\n'))
-    if input_temperature == 373:
-        print('The temperature that was inputted was: ', input_temperature,'K')
-        break
-    elif input_temperature == 423:
-        print('The temperature that was inputted was: ', input_temperature,'K')
-        break
-    elif input_temperature == 473:
-        print('The temperature that was inputted was: ', input_temperature,'K')
-        break  
-    elif input_temperature == 523:
-        print('The temperature that was inputted was: ', input_temperature,'K')
-        break
-    elif input_temperature == 573:
-        print('The temperature that was inputted was: ', input_temperature,'K')
-        break
-    else:
-        print('This is an invalid input. Please try again.')
-        
+' 1. add the hydrolysis expressions into the function skeleton provided below' 
+' output (returns) should be the hydrolysis constants'
+' 2. call the function at a few different temperatures' 
+' (no need to write fancy print output)'
+'3. make a list/array of temperatures to test and iterate calling function through it' 
+'4. make an empty array to save (append) the eventual solutions of the function'
+'5. loop through the input temperature list and append solutions to just one of' 
+' the hyrolysis constants to it'
+'6. print the list' 
+
+def iron_hydrolysis_constants(Temp):
+    
+    
+    
+    return None
+      
+
 LOG_K_FeOH_hydr = np.poly1d(KFeOHPOLYNOMIAL)
 LOG_K_FeOH2_hydr = np.poly1d(KFeOH2POLYNOMIAL)
 LOG_K_FeOH3_hydr = np.poly1d(KFeOH3POLYNOMIAL)
 
-K_FeOH_hydr = 10 ** (LOG_K_FeOH_hydr(input_temperature))
-K_FeOH_hydr = 10 ** (LOG_K_FeOH_hydr(input_temperature))
-K_FeOH_hydr = 10 ** (LOG_K_FeOH_hydr(input_temperature))
+K_FeOH_hydr = 10 ** LOG_K_FeOH_hydr(Temperature_K)
+K_FeOH_hydr = 10 ** LOG_K_FeOH_hydr(Temperature_K)
+K_FeOH_hydr = 10 ** LOG_K_FeOH_hydr(Temperature_K)
 
-print('\n' 'At this temperature, the equilibrium constants' '\n'
-    'for FeOH, FeOH2 and FeOH3, respectively,' '\n'
-    'from the Tremaine & LeBlanc paper using the new method, poly1d, are:')
-    
-print(K_FeOH_hydr)
-print(K_FeOH2_hydr)
-print(K_FeOH3_hydr)
+
+
+
+
+
+
